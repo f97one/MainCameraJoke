44f4b23ee6975569873f114c7a9f1d1dad288542
HAJIME Fukuna <f97one@gmail.com>
2014-01-18 23:11:23 +0900

    カメラIDを取得するメソッドのスタブを追加。

--- app_MainCameraJoke/src/main/java/net/formula97/android/app_maincamerajoke/CamView.java
+++ app_MainCameraJoke/src/main/java/net/formula97/android/app_maincamerajoke/CamView.java
@@ -4,4 +4,11 @@ package net.formula97.android.app_maincamerajoke;
  * Created by HAJIME on 14/01/18.
  */
 public class CamView {
+    public CamView() {
+
+    }
+    public int getCamId(int cam) {
+
+        return -1;  // not implemented
+    }
 }
