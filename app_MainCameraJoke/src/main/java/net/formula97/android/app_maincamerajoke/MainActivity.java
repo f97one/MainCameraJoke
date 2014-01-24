ff674ab7ff3f1a9570caca989bcafe5116aeb240
HAJIME Fukuna <f97one@gmail.com>
2014-01-25 00:44:11 +0900

    SurfaceHolder.Callbackを実装する方針に変更。

--- app_MainCameraJoke/src/main/java/net/formula97/android/app_maincamerajoke/MainActivity.java
+++ app_MainCameraJoke/src/main/java/net/formula97/android/app_maincamerajoke/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.ViewGroup;
 /**
  *
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SurfaceHolder.Callback {
 
     private Camera mCamera;
     private CamView camView;
@@ -97,9 +97,54 @@ public class MainActivity extends ActionBarActivity {
             Camera.getCameraInfo(i, info);
             if (info.facing == camId) {
                 c = Camera.open(i);
+                break;
             }
         }
         return c;
+    }
+
+    /**
+     * This is called immediately after the surface is first created.
+     * Implementations of this should start up whatever rendering code
+     * they desire.  Note that only one thread can ever draw into
+     * a {@link Surface}, so you should not draw into the Surface here
+     * if your normal rendering will be in another thread.
+     *
+     * @param holder The SurfaceHolder whose surface is being created.
+     */
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    /**
+     * This is called immediately after any structural changes (format or
+     * size) have been made to the surface.  You should at this point update
+     * the imagery in the surface.  This method is always called at least
+     * once, after {@link #surfaceCreated}.
+     *
+     * @param holder The SurfaceHolder whose surface has changed.
+     * @param format The new PixelFormat of the surface.
+     * @param width  The new width of the surface.
+     * @param height The new height of the surface.
+     */
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    /**
+     * This is called immediately before a surface is being destroyed. After
+     * returning from this call, you should no longer try to access this
+     * surface.  If you have a rendering thread that directly accesses
+     * the surface, you must ensure that thread is no longer touching the
+     * Surface before returning from this function.
+     *
+     * @param holder The SurfaceHolder whose surface is being destroyed.
+     */
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 
     /**
