0df7db906123361a0ceaef8e5cdce4d045167cc9
HAJIME Fukuna <f97one@gmail.com>
2014-06-19 21:24:36 +0900

    ユーザー定義のコンストラクタを追加。

--- app_MainCameraJoke/src/main/java/net/formula97/android/app_maincamerajoke/DatabaseHelper.java
+++ app_MainCameraJoke/src/main/java/net/formula97/android/app_maincamerajoke/DatabaseHelper.java
@@ -5,18 +5,38 @@ import android.database.sqlite.SQLiteDatabase;
 
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
 
 /**
+ * データベース操作用のクラス。
+ *
  * Created by HAJIME on 2014/06/19.
  */
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
+
     public DatabaseHelper(Context context, String databaseName, SQLiteDatabase.CursorFactory factory, int databaseVersion) {
         super(context, databaseName, factory, databaseVersion);
     }
 
+    /**
+     * ユーザー定義のコンストラクタ。<br />
+     * Context以外は定数を引き渡すことにしている。
+     *
+     * @param context コンテキスト
+     */
+    public DatabaseHelper(Context context) {
+        super(context, Constants.DATABASE_NAME, null, Constants.DATABASE_VERSION);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase, ConnectionSource connectionSource) {
-
+        try {
+            TableUtils.createTable(connectionSource, NetaMessages.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
