636b20a08a94253abddb9f4ccfe497417908d291
HAJIME Fukuna <f97one@gmail.com>
2014-07-31 22:12:26 +0900

    ネタ文言を追加する処理を追加。

--- app_MainCameraJoke/src/main/java/net/formula97/android/app_maincamerajoke/AddNetaDialogFragment.java
+++ app_MainCameraJoke/src/main/java/net/formula97/android/app_maincamerajoke/AddNetaDialogFragment.java
@@ -8,7 +8,10 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
+import java.sql.SQLException;
 import java.util.EventListener;
 
 
@@ -60,7 +63,9 @@ public class AddNetaDialogFragment extends DialogFragment {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         LayoutInflater layoutInflater = activity.getLayoutInflater();
-        builder.setView(layoutInflater.inflate(R.layout.fragment_add_neta_dialog, null));
+        View view = layoutInflater.inflate(R.layout.fragment_add_neta_dialog, null);
+        builder.setView(view);
+        final EditText editText = (EditText) view.findViewById(R.id.editText);
         builder.setTitle(R.string.add_neta);
         builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
             @Override
@@ -77,6 +82,13 @@ public class AddNetaDialogFragment extends DialogFragment {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 // TODO DBに追加する処理を書く
+                NetaMessages neta = new NetaMessages(editText.getText().toString());
+                NetaMessagesModel model = new NetaMessagesModel(activity);
+                try {
+                    model.save(neta);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
 
                 if (callback != null) {
                     // ダイアログを閉じたことを通知する
