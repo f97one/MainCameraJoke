4ffb6f5e646c1a187a25f57cd76b5af61041569a
HAJIME Fukuna <f97one@gmail.com>
2014-07-31 22:11:06 +0900

    検索についての処理を追加。

--- app_MainCameraJoke/src/main/java/net/formula97/android/app_maincamerajoke/BaseModel.java
+++ app_MainCameraJoke/src/main/java/net/formula97/android/app_maincamerajoke/BaseModel.java
@@ -7,6 +7,7 @@ import com.j256.ormlite.dao.Dao;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by HAJIME on 2014/06/22.
@@ -25,6 +26,7 @@ public class BaseModel {
     public BaseModel(Context context) {
         mContext = context;
     }
+
     /**
      * DatabaseHelperのインスタンスを得る。
      *
@@ -61,6 +63,14 @@ public class BaseModel {
         dao.createOrUpdate(entity);
     }
 
+    /**
+     * エンティティのリストをまとめてデータベースに保存する。<br />
+     * データベースに該当レコードがある場合はUpdate、ない場合はInsertされる。
+     *
+     * @param entities データベースに登録したいエンティティの集合
+     * @param <T>
+     * @throws SQLException
+     */
     public <T> void save(List<?> entities) throws SQLException {
         DatabaseHelper helper = getHelper();
 
@@ -71,6 +81,14 @@ public class BaseModel {
         }
     }
 
+    /**
+     * 指定したエンティティを削除する。
+     *
+     * @param entity 削除したいエンティティ
+     * @param <T>
+     * @return 影響のあった（＝削除された）レコード数
+     * @throws SQLException
+     */
     public <T> int delete(T entity) throws SQLException {
         DatabaseHelper helper = getHelper();
 
@@ -78,6 +96,14 @@ public class BaseModel {
         return dao.delete(entity);
     }
 
+    /**
+     * エンティティのテーブルに格納されているレコードをすべて取得する。
+     *
+     * @param entity 取得したいエンティティ
+     * @param <T>
+     * @return テーブルに格納されている全レコードの集合
+     * @throws SQLException
+     */
     public <T> List<?> findAll(T entity) throws SQLException {
         DatabaseHelper helper = getHelper();
 
@@ -86,5 +112,36 @@ public class BaseModel {
         return dao.queryForAll();
     }
 
+    /**
+     * 単一のフィールドに指定した値を持つレコードを取得する。
+     *
+     * @param entity 取得したいエンティティ
+     * @param fieldName 検査するフィールド名称
+     * @param value 検査対象の値
+     * @param <T>
+     * @return 条件に合致するレコードの集合
+     * @throws SQLException
+     */
+    public <T> List<?> findBySingleArg(T entity, String fieldName, Object value) throws SQLException {
+        DatabaseHelper helper = getHelper();
+        Dao<T, Integer> dao = helper.getDao((Class<T>) entity.getClass());
 
+        return dao.queryForEq(fieldName, value);
+    }
+
+    /**
+     * フィールドと値のセットに該当するレコードを取得する。
+     *
+     * @param entity 取得したいエンティティ
+     * @param fieldValues フィールドと値のセット
+     * @param <T>
+     * @return 条件に合致するレコードの集合
+     * @throws SQLException
+     */
+    public <T> List<?> findByFieldValuesArgs(T entity, Map<String, Object> fieldValues) throws SQLException {
+        DatabaseHelper helper = getHelper();
+        Dao<T, Integer> dao = helper.getDao((Class<T>) entity.getClass());
+
+        return dao.queryForFieldValuesArgs(fieldValues);
+    }
 }
